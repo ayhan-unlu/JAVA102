fb1c6781e79f48ace3d24d809355f44f3a41b51d
Ayhan Unlu <ayhan-unlu@hotmail.com>
2024-07-05 20:48:36 +0300

    Inner Classes Begun

---
+++ src/Nested_Inner_Classes_240705/Anonym.java
@@ -0,0 +1,9 @@
+package Nested_Inner_Classes_240705;
+
+public class Anonym {
+    public int a=45;
+    public void run(){
+        System.out.println("Anonym Class Run Method is Executed");
+    }
+
+}
---
+++ src/Nested_Inner_Classes_240705/Out.java
@@ -0,0 +1,34 @@
+package Nested_Inner_Classes_240705;
+// Outer Class
+public class Out {
+    public int a=5;
+    public static int b=20;
+
+// Inner Class
+    public static class In{
+         public static int a=10;
+         
+         public static void run(){
+            System.out.println("Inner Class Run Method is Executed");
+            int a=1;
+            System.out.println(a);
+            System.out.println(a);
+            System.out.println(In.a);
+            System.out.println(Out.b);
+            //System.out.println(Out.this.a);
+        /// recursive    Out.this.run();
+         }
+    }
+    public void run(){
+        //In in=new In();
+        //XXXX System.out.println(in.run());
+        In.run();
+        ///System.out.println(In.a);
+
+    }
+//    public In getIn(){
+  //      In in = new In();
+    //    return in;
+  //  }
+///
+}
