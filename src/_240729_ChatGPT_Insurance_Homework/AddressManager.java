51712f9dc6566c9ff83e67d7a3b73346f54bfe86
Ayhan Unlu <ayhan-unlu@hotmail.com>
2024-07-29 15:57:46 +0300

    Some changes are made on chatGPT solution

---
+++ src/_240729_ChatGPT_Insurance_Homework/AddressManager.java
@@ -0,0 +1,11 @@
+package _240729_ChatGPT_Insurance_Homework;
+
+class AddressManager {
+    public static void addAddress(User user, Address address) {
+        user.addAddress(address);
+    }
+
+    public static void removeAddress(User user, Address address) {
+        user.removeAddress(address);
+    }
+}
---
+++ src/_240729_ChatGPT_Insurance_Homework/InvalidAuthentictionException.java
@@ -0,0 +1,6 @@
+package _240729_ChatGPT_Insurance_Homework;
+class InvalidAuthenticationException extends Exception {
+    public InvalidAuthenticationException(String message) {
+        super(message);
+    }
+}
