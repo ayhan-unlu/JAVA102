538b9a52aa77d798939608695c32d854a9d2bc0f
Ayhan Unlu <ayhan-unlu@hotmail.com>
2024-12-16 13:11:01 +0300

    Revert "checkpoint241216"
    This reverts commit 0fd935f3e74828caac3148691fadc581688c57f9.

--- src/_241308/com/patikadev/View/EducatorGUI.java
+++ src/_241308/com/patikadev/View/EducatorGUI.java
@@ -11,7 +11,6 @@ public class EducatorGUI extends JFrame {
     private JLabel label_educator_welcome;
     private JButton button_logout;
     private JTabbedPane tab_educator;
-    private JPanel panel_top;
     private final Educator educator;
 
     public EducatorGUI(Educator educator) {
