b8ff0f703c2c9f02016d0af75d0949bb4a5bb84d
Ayhan Unlu <ayhan-unlu@hotmail.com>
2024-12-22 16:48:19 +0300

    1. Educator menu content panel list added. 2. Content add menu added only isFieldEmpty check is completed.

--- src/_241308/com/patikadev/Model/Content.java
+++ src/_241308/com/patikadev/Model/Content.java
@@ -1,23 +1,110 @@
 package _241308.com.patikadev.Model;
 
+import _241308.com.patikadev.Helper.DBConnector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class Content {
+
     private int id;
-    private String title;
-    private String detail;
-    private String youtubeLink;
-    private String quizQuestions;
+    private String name;
+    private String info;
+    private String youtube_link;
+    private String quiz_questions;
     private int course_id;
-    private int user_id;
 
     private Course course;
-    private User educator;
 
 
-    public Content() {
+    public Content(int id, String name, String info, String youtube_link, String quiz_questions, int course_id) {
+        this.id = id;
+        this.name = name;
+        this.info = info;
+        this.youtube_link = youtube_link;
+        this.quiz_questions = quiz_questions;
+        this.course = Course.getFetch(course_id);
     }
 
-    Content (int id, String title,String detail,String youtubeLink,String quizQuestions, int course_id,int user_id){
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getYoutube_link() {
+        return youtube_link;
+    }
+
+    public void setYoutube_link(String youtube_link) {
+        this.youtube_link = youtube_link;
+    }
+
+    public String getQuiz_questions() {
+        return quiz_questions;
+    }
+
+    public void setQuiz_questions(String quiz_questions) {
+        this.quiz_questions = quiz_questions;
+    }
+
+    public int getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(int course_id) {
+        this.course_id = course_id;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public static ArrayList<Content> getList() {
+        ArrayList<Content> contentList = new ArrayList<>();
+        Content obj;
+
+        try {
+            Statement st = DBConnector.getInstance().createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM content");
+            while(rs.next()){
+                int id=rs.getInt("id");
+                String name=rs.getString("name");
+                String info=rs.getString("info");
+                String youtube_link=rs.getString("youtube_link");
+                String quiz_questions=rs.getString("quiz_questions");
+                int course_id=rs.getInt("course_id");
+                obj = new Content(id,name,info,youtube_link,quiz_questions,course_id);
+                contentList.add(obj);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return contentList;
     }
 
 }
--- src/_241308/com/patikadev/View/EducatorGUI.java
+++ src/_241308/com/patikadev/View/EducatorGUI.java
@@ -2,6 +2,7 @@ package _241308.com.patikadev.View;
 
 import _241308.com.patikadev.Helper.Config;
 import _241308.com.patikadev.Helper.Helper;
+import _241308.com.patikadev.Model.Content;
 import _241308.com.patikadev.Model.Course;
 import _241308.com.patikadev.Model.Educator;
 
@@ -17,15 +18,32 @@ public class EducatorGUI extends JFrame {
     private JPanel panel_educator_course_list;
     private JScrollPane scroll_educator_course_list;
     private JTable table_educator_course_list;
+    private JPanel panel_educator_content_list;
+    private JScrollPane scroll_educator_content_list;
+    private JTable table_educator_content_list;
+    private JPanel panel_educator_content_add;
+    private JTextField field_educator_content_add_name;
+    private JLabel label_educator_content_add_name;
+    private JLabel label_educator_content_add_info;
+    private JTextField field_educator_content_add_info;
+    private JLabel label_educator_content_add_youtube_link;
+    private JTextField field_educator_content_add_youtube_link;
+    private JLabel label_educator_content_add_quiz_questions;
+    private JTextField field_educator_content_add_quiz_questions;
+    private JLabel label_educator_content_add_course_id;
+    private JComboBox combobox_educator_content_add_course_id;
+    private JButton button_educator_content_add;
     private final Educator educator;
     private DefaultTableModel model_educator_course_list;
     private Object[] row_educator_course_list;
+    private DefaultTableModel model_educator_content_list;
+    private Object[] row_educator_content_list;
 
     public EducatorGUI(Educator educator) {
         //check point 5
         this.educator = educator;
         add(wrapper);
-        setSize(400, 400);
+        setSize(1000, 500);
         setLocation(Helper.screenCenterPoint("x", getSize()), Helper.screenCenterPoint("y", getSize()));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle(Config.PROJECT_TITLE);
@@ -43,18 +61,36 @@ public class EducatorGUI extends JFrame {
         // Object[] first_educator_course_row = {"1", "Path Clone", "Mustafa Çetindağ", "Java102", "Java"};
         //model_educator_course_list.addRow(first_educator_course_row);
         for (Course obj : Course.getListByUser(educator.getId())) {
-            Object [] row = new Object[column_educator_course_list.length];
-            row[0]=obj.getId();
-            row[1]=obj.getName();
-            row[2]=obj.getEducator().getName();
-            row[3]=obj.getPath().getName();
-            row[4]=obj.getLanguage();
+            Object[] row = new Object[column_educator_course_list.length];
+            row[0] = obj.getId();
+            row[1] = obj.getName();
+            row[2] = obj.getEducator().getName();
+            row[3] = obj.getPath().getName();
+            row[4] = obj.getLanguage();
             model_educator_course_list.addRow(row);
         }
+
         table_educator_course_list.setModel(model_educator_course_list);
 
         table_educator_course_list.getTableHeader().setReorderingAllowed(false);
         table_educator_course_list.getColumnModel().getColumn(0).setMaxWidth(30);
+
+        //##ModelEducatorCourseList
+
+        //ModelEducatorContentList
+        model_educator_content_list = new DefaultTableModel();
+        Object [] column_educator_content_list={"id","name","info","youtube_link","quiz_questions","course_id"};
+        model_educator_content_list.setColumnIdentifiers(column_educator_content_list);
+        row_educator_content_list = new Object[column_educator_content_list.length];
+
+        loadContentList();
+        table_educator_content_list.setModel(model_educator_content_list);
+        table_educator_content_list.getColumnModel().getColumn(0).setMaxWidth(75);
+        table_educator_content_list.getTableHeader().setReorderingAllowed(false);
+
+
+
+        //##ModelEducatorContentList
 
 
         button_logout.addActionListener(e -> {
@@ -62,5 +98,29 @@ public class EducatorGUI extends JFrame {
             LoginGUI loginGUI = new LoginGUI();
 
         });
+        button_educator_content_add.addActionListener(e -> {
+            if(Helper.isFieldEmpty(field_educator_content_add_name)||Helper.isFieldEmpty(field_educator_content_add_info)||Helper.isFieldEmpty(field_educator_content_add_info)||Helper.isFieldEmpty(field_educator_content_add_youtube_link)||Helper.isFieldEmpty(field_educator_content_add_quiz_questions)){
+                Helper.showMessage("fill");
+            }
+
+        });
+    }
+
+    private void loadContentList() {
+
+        DefaultTableModel clearModel= (DefaultTableModel)table_educator_content_list.getModel();
+        clearModel.setRowCount(0);
+        int i=0;
+
+        for(Content obj: Content.getList()){
+            i=0;
+            row_educator_content_list[i++]=obj.getId();
+            row_educator_content_list[i++]=obj.getName();
+            row_educator_content_list[i++]=obj.getInfo();
+            row_educator_content_list[i++]=obj.getYoutube_link();
+            row_educator_content_list[i++]=obj.getQuiz_questions();
+            row_educator_content_list[i++]=obj.getCourse().getId();
+            model_educator_content_list.addRow(row_educator_content_list);
+        }
     }
 }
