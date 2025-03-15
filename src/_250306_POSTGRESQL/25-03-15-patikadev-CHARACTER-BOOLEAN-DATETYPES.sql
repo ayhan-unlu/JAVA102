25713a352451ec4b3f52bb78f38daca105fb8ac5
Ayhan Unlu <ayhan-unlu@hotmail.com>
2025-03-15 16:17:26 +0300

    1. POSTGRESQL DATA TYPES CHARACTER-BOOLEAN-DATETYPES notes uploaded-

---
+++ src/_250306_POSTGRESQL/25-03-15-patikadev-CHARACTER-BOOLEAN-DATETYPES.sql
@@ -0,0 +1,59 @@
+-- CHARACTER TYPES
+
+--SELECT ('Lorem'::CHAR(10));
+
+--SELECT ('Lorem ipsum dolor sit amet'::CHAR(10));
+
+--SELECT ('Lorem'::VARCHAR(10));
+
+--SELECT ('Lorem ipsum dolor sit amet'::VARCHAR(10));
+
+--SELECT ('Lorem ipsum dolor sit amet'::VARCHAR);
+
+--SELECT ('Lorem'::TEXT);
+
+--SELECT ('Lorem ipsum dolor sit amet'::TEXT);
+
+--BOOLEAN TYPES
+
+--SELECT (true);
+--SELECT ('yes');
+--SELECT ('t' :: boolean);
+--SELECT (1 :: boolean);
+
+--SELECT (false);
+
+--SELECT ('no'::boolean);
+--SELECT ('f'::boolean);
+--SELECT (0 :: boolean);
+
+--SELECT (null);
+
+--SELECT (null::Boolean);
+
+--DATE / TIME TYPES
+
+--SELECT ('1980-12-03'::DATE);
+
+--SELECT ('DEC-03-1980'::DATE);
+
+--SElECT ('DEC 03 1980'::DATE);
+
+--SELECT ('1980 December 03'::DATE);
+
+--SELECT ('03:44'::TIME);
+
+--SELECT ('03:44 AM'::TIME);
+
+--SELECT ('03:44 PM'::TIME);
+
+--SELECT ('03:44:11 PM'::TIME);
+
+--SELECT('03:44::TIME WITH TIMEZONE');
+
+--SELECT('03:44'::TIME WITH TIME ZONE);
+
+--SELECT ('1980 December 03 02:16:17'::TIMESTAMP);
+
+SELECT ('1980 December 03 02:16:17 PM'::TIMESTAMP WITH TIME ZONE);
+
